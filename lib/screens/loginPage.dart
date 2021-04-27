f4b126cd00f9175f165526e734f4e793c880042d
= <=>
2021-04-27 23:58:14 +0530

    Update loginPage.dart

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'signupPage.dart';
 
 class LoginPage extends StatefulWidget {
@@ -36,10 +35,10 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   String email;
-  String pass;
+  String Password;
 
   final emailCon = new TextEditingController();
-  final passCon = new TextEditingController();
+  final PasswordCon = new TextEditingController();
 
   bool visi1 = true;
   IconData i1 = Icons.visibility;
@@ -64,113 +63,117 @@ class _LoginPageState extends State<LoginPage> {
         ),
         body: SingleChildScrollView(
           // helps to scroll the page and doesn't give error of cannot display screen
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Hero(
-                  tag: 'logo',
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage(
-                      'images/ECHOS2.png',
-                    ),
-                  ),
-                ),
-              ),
-              Text(
-                'Echos',
-                style: TextStyle(
-                  fontFamily: 'DancingScript',
-                  fontSize: 20,
-                  // fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                child: TextField(
-                  controller: emailCon,
-                  style: TextStyle(color: Colors.black),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: new InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color(0xFFC3C2C3), width: 2.0),
-                    ),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(color: Colors.black54),
-                  ),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                child: TextField(
-                  obscureText: visi1,
-                  controller: passCon,
-                  style: TextStyle(color: Colors.black),
-                  decoration: new InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color(0xFFC3C2C3), width: 2.0),
-                    ),
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color: Colors.black54),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        setState(() {
-                          visi1 = !visi1;
-                          if (visi1)
-                            i1 = Icons.visibility;
-                          else
-                            i1 = Icons.visibility_off;
-                        });
-                      },
-                      child: Icon(
-                        i1,
-                        color: Colors.black45,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Hero(
+                    tag: 'logo',
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(
+                        'images/ECHOS2.png',
                       ),
                     ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    email = emailCon.text;
-                    pass = passCon.text;
-                    print('Email= $email');
-                    print('Password= $pass');
-                  });
-                },
-                child: Text('Login'),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, SignupPage.id);
-                },
-                child: Text(
-                  'Don\'t have an account? Sign Up',
+                Text(
+                  'Echos',
                   style: TextStyle(
-                      color: Colors.lightBlue[700],
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 15),
+                    fontFamily: 'DancingScript',
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: TextField(
+                    controller: emailCon,
+                    style: TextStyle(color: Colors.black),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFFC3C2C3), width: 2.0),
+                      ),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(color: Colors.black45),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: TextFormField(
+                    obscureText: visi1,
+                    controller: PasswordCon,
+                    style: TextStyle(color: Colors.black),
+                    decoration: new InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFFC3C2C3), width: 2.0),
+                      ),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Colors.black45),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            visi1 = !visi1;
+                            if (visi1)
+                              i1 = Icons.visibility;
+                            else
+                              i1 = Icons.visibility_off;
+                          });
+                        },
+                        child: Icon(
+                          i1,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //_formKey.currentState.validate();
+                    setState(() {
+                      if (_formKey.currentState.validate()) {
+                        email = emailCon.text;
+                        Password = PasswordCon.text;
+
+                        print('$email, $Password');
+                      }
+                    });
+                  },
+                  child: Text('Log In',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, SignupPage.id);
+                  },
+                  child: Text(
+                    'Don\'t have an account? Sign Up',
+                    style: TextStyle(
+                        color: Colors.lightBlue[700],
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 15),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         // ),
