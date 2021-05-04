8f4a537c62751a431245d8f8f9bc91258e6e298c
PrayagS <prayag.s@ahduni.edu.in>
2021-05-04 14:03:49 +0530

    feat: setup camera interface

--- lib/screens/aslDetection.dart
+++ lib/screens/aslDetection.dart
@@ -1,106 +1,179 @@
-import 'package:flutter/cupertino.dart';
+import 'package:sign_language_tutor/helpers/logging.dart';
+import 'package:sign_language_tutor/helpers/cameraHelper.dart';
+import 'package:sign_language_tutor/helpers/TFLiteHelper.dart';
+import 'package:sign_language_tutor/models/result.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
-import '../rewidgets/navBar.dart';
-import 'loginPage.dart';
+class ASLDetection extends StatefulWidget {
+  static String id = 'detection_page';
+  @override
+  _ASLDetectionState createState() => _ASLDetectionState();
+}
 
-class ASLDetection extends StatelessWidget {
-  static String id = 'home_page';
+class _ASLDetectionState extends State<ASLDetection>
+    with TickerProviderStateMixin {
+  AnimationController _colorAnimController;
+  Animation _colorTween;
+  final FlutterTts flutterTts = FlutterTts();
+
+  List<Result> outputs;
+
+  void initState() {
+    super.initState();
+
+    //Load TFLite Model
+    TFLiteHelper.loadModel().then((value) {
+      setState(() {
+        TFLiteHelper.modelLoaded = true;
+      });
+    });
+
+    //Initialize Camera
+    CameraHelper.initializeCamera();
+
+    //Setup Animation
+    _setupAnimation();
+
+    //Subscribe to TFLite's Classify events
+    TFLiteHelper.tfLiteResultsController.stream.listen(
+            (value) {
+          value.forEach((element) {
+            _colorAnimController.animateTo(element.confidence,
+                curve: Curves.bounceIn, duration: Duration(milliseconds: 500));
+          });
+
+          //Set Results
+          outputs = value;
+
+          //Update results on screen
+          setState(() {
+            //Set bit to false to allow detection again
+            CameraHelper.isDetecting = false;
+          });
+        },
+        onDone: () {},
+        onError: (error) {
+          AppHelper.log("listen", error);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      // child: Container(
-      //   decoration: BoxDecoration(
-      //     gradient: LinearGradient(
-      //         begin: Alignment.bottomLeft,
-      //         end: Alignment.topRight,
-      //         colors: [Colors.black, Colors.blue]),
-      //   ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Home',
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: true,
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
         ),
-        bottomNavigationBar: NavBar(
-          id: ASLDetection.id,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: [
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Tutorials',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        child: Card(
-                          // with Material
-                          child: Image.asset(
-                            'assets/images/Tutorial.png',
-                          ),
-                          elevation: 18.0,
-                          clipBehavior: Clip.antiAlias,
-                          color: Color(0xFFd3b4b3),
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.pushNamed(context, LoginPage.id);
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Translate',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        child: Card(
-                          // with Material
-                          child: Image.asset(
-                            'assets/images/LogoEchoes.png',
-                          ),
-                          elevation: 18.0,
-                          clipBehavior: Clip.antiAlias,
-                          color: Color(0xFFf7f3e9),
-                        ),
-                      ),
-                    ),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        // ),
+      ),
+      body: FutureBuilder<void>(
+        future: CameraHelper.initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return Stack(
+              children: <Widget>[
+                CameraPreview(CameraHelper.camera),
+                _buildResultsWidget(width, outputs)
+              ],
+            );
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    TFLiteHelper.disposeModel();
+    CameraHelper.camera.dispose();
+    AppHelper.log("dispose", "Clear resources.");
+    super.dispose();
+  }
+
+  Widget _buildResultsWidget(double width, List<Result> outputs) {
+    Future speak(String s) async {
+      await flutterTts.setLanguage("en-US");
+      await flutterTts.setPitch(1);
+      await flutterTts.setSpeechRate(0.5);
+      await flutterTts.speak(s);
+    }
+
+    return Positioned.fill(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 200.0,
+          width: width,
+          color: Colors.white,
+          child: outputs != null && outputs.isNotEmpty
+              ? ListView.builder(
+              itemCount: outputs.length,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20.0),
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    Text(
+                      outputs[index].label,
+                      style: TextStyle(
+                        color: _colorTween.value,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    AnimatedBuilder(
+                        animation: _colorAnimController,
+                        builder: (context, child) => LinearPercentIndicator(
+                          width: width * 0.88,
+                          lineHeight: 14.0,
+                          percent: outputs[index].confidence,
+                          progressColor: _colorTween.value,
+                        )),
+                    Text(
+                      "${(outputs[index].confidence * 100.0).toStringAsFixed(2)} %",
+                      style: TextStyle(
+                        color: _colorTween.value,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Center(
+                      child: FlatButton(
+                        onPressed: () {
+                          speak("${outputs[index].label}");
+                        },
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 60,
+                          color: Color(0xff375079),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              })
+              : Center(
+              child: Text("Waiting for model to detect..",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                  ))),
+        ),
+      ),
+    );
+  }
+
+  void _setupAnimation() {
+    _colorAnimController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _colorTween = ColorTween(begin: Colors.green, end: Colors.red)
+        .animate(_colorAnimController);
   }
 }
